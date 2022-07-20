3922162b50ae30d50dce54b83b5ed4a96e6b2c0e
davey <davey89451@gmail.com>
2022-07-20 14:27:12 -0700

    repl commit

---
+++ src/front-end/animation.js
@@ -0,0 +1,100 @@
+const elts = {
+  text1: document.getElementById("text1"),
+  text2: document.getElementById("text2"),
+};
+
+const texts = [
+  "If",
+  "You",
+  "Like",
+  "It",
+  "Please",
+  "Give",
+  "a Love",
+  ":)",
+  "by @DotOnion",
+];
+
+const morphTime = 1;
+const cooldownTime = 0.25;
+
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+  morph -= cooldown;
+  cooldown = 0;
+
+  let fraction = morph / morphTime;
+
+  if (fraction > 1) {
+    cooldown = cooldownTime;
+    fraction = 1;
+  }
+
+  setMorph(fraction);
+}
+
+function setMorph(fraction) {
+  elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  fraction = 1 - fraction;
+  elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  elts.text1.textContent = texts[textIndex % texts.length];
+  elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+  morph = 0;
+
+  elts.text2.style.filter = "";
+  elts.text2.style.opacity = "100%";
+
+  elts.text1.style.filter = "";
+  elts.text1.style.opacity = "0%";
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  let newTime = new Date();
+  let shouldIncrementIndex = cooldown > 0;
+  let dt = (newTime - time) / 1000;
+  time = newTime;
+
+  cooldown -= dt;
+
+  if (cooldown <= 0) {
+    if (shouldIncrementIndex) {
+      textIndex++;
+    }
+
+    doMorph();
+  } else {
+    doCooldown();
+  }
+}
+
+animate();
+
+var menuBtn = document.querySelector(".menu-btn");
+var nav = document.querySelector("nav");
+var lineOne = document.querySelector("nav .menu-btn .line--1");
+var lineTwo = document.querySelector("nav .menu-btn .line--2");
+var lineThree = document.querySelector("nav .menu-btn .line--3");
+var link = document.querySelector("nav .nav-links");
+menuBtn.addEventListener("click", () => {
+  nav.classList.toggle("nav-open");
+  lineOne.classList.toggle("line-cross");
+  lineTwo.classList.toggle("line-fade-out");
+  lineThree.classList.toggle("line-cross");
+  link.classList.toggle("fade-in");
+});
